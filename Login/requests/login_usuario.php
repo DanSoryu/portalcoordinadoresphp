c39eb470e16b2108b720947f66422dca78669be5
zanwonzzzz <etnicastro9@gmail.com>
2025-09-30 14:23:45 -0600

    XD

--- Login/requests/login_usuario.php
+++ Login/requests/login_usuario.php
@@ -1,7 +1,7 @@
 <?php
-// Habilitar la salida de errores a la página
+// Deshabilitar la salida de errores a la página
 error_reporting(E_ALL);
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/error.log');
 
